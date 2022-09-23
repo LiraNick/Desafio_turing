89b969f5fb5aead1d527b1eaf2ab018052609b53
Nicolas Lira Galiano <nicolas.galiano@proway.treina>
2022-09-23 19:31:25 -0300

    atualizando arquivos

---
+++ parte03.py
@@ -0,0 +1,25 @@
+cond = "sim"
+while cond == "sim":
+
+    var = int(input("Digite um numero: "))
+    print(f"O nome Digitado foi {cond}")
+    cond = str(input("Deseja Continuar?  \n sim  \n nao  \n :>"))
+print("Voce foi eliminado")
+
+
+var = 1 
+while var != 0:
+    var = int(input("digite um numero: "))
+    soma = var +1
+    print(f"o numero digitado é {var} o adicional do numero ficou em {soma}")
+print("Voce foi eliminado")
+
+validador = True
+
+validador = False
+
+idade = int(input("Digite sua idade: "))
+
+
+validador = ( idade == 18)
+print(validador)
